a5a3f2455d644af929616e362c139ad984e9ad28
Ayushi <ayushi2530@gmail.com>
2026-02-25 15:19:19 +0800

    Initial MVP version

--- scripts/analyze_errors.sh
+++ scripts/analyze_errors.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-LOG_FILE="../logs/app.log"
+echo "========== Log Analysis Summary =========="
+LOG_FILE=$1
+echo "Verifying log file destination..."
+sleep 3
+echo "Reading the file $1..."
+sleep 3
+echo "Please note: ERROR_THRESHOLD=20"
+sleep 3
+if [ ! -e "$1" ]; then
+  echo "[ERROR] log file/path is incorrect: $1"
+  exit 2
+fi
 
-echo "===== ERROR SUMMARY ====="
-grep "ERROR" $LOG_FILE | awk '{print $2}' | sort | uniq -c | sort -nr
+lines=$(wc -l <"$LOG_FILE")
+echo "LINES Count: $lines"
 
-echo ""
-echo "Total ERROR count:"
-grep -c "ERROR" $LOG_FILE
+errors=$(grep "ERROR" "$LOG_FILE" | wc -l)
+echo "ERROR Count: $errors"
+
+warning=$(grep "WARNING" "$LOG_FILE" | wc -l)
+echo "WARNING Count: $warning"
+
+critical=$(grep "CRITICAL" "$LOG_FILE" | wc -l)
+echo "CRITICAL Count: $critical"
+
+if [[ $errors -ge 20 ]]; then
+	echo "ANOMOLY DETECTED, ERROR >= 20"
+else
+	echo "SYSTEM IS GOOD"
+fi
+
